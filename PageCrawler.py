73934e8b40adc88f23456fe41bf6805437a245fc
Nathan TeBlunthuis <nathante@uw.edu>
2020-04-03 15:19:23 -0700

    Organize wikidata crawling
    - Make some obvious code-reuse
    - Adopt a more functional style in places
    - use if __name__=="__main__" to make functions importable without running everything.

--- PageCrawler.py
+++ PageCrawler.py
@@ -3,7 +3,6 @@
 
 # # COVID-19 All Related articles (and tagged relation) tracker
 
-
 # 
 # First we take all Wikidata Articles that links to a main  COVID-19 pages [COVID-19 (Q84263196)](https://www.wikidata.org/wiki/Q84263196) ;  SARS-CoV-2 (Q82069695) and   [2019–20 COVID-19 pandemic (Q81068910)](https://www.wikidata.org/wiki/Q81068910), and then we join both sets and do a final pass to get relationships between the seeds and all the resulting items.
 
@@ -11,143 +10,95 @@
 #Note that the output is saved on SQLite database
 #Results are optimized for DB
 
-
 import requests
 from SPARQLWrapper import SPARQLWrapper, JSON
 import pandas as pd
 import sqlite3
+from tqdm import tqdm
+from functools import reduce
+from itertools import chain
 
 #get crawling timestamp
 now  = pd.Timestamp.now()
-
-
+sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+wikidata_query_base = 'https://www.wikidata.org/w/api.php?action=wbgetentities&format=json&ids=' 
 
 # pages linking COVID-19 wikidata item Q84263196
 # whatlinks approach
-whatLinks = []
-url = 'https://www.wikidata.org/w/api.php?action=query&format=json&list=backlinks&bltitle=Q84263196&bllimit=500&blnamespace=0'
-response = requests.get(url=url).json()
-whatLinks.extend(response['query']['backlinks'])
-
-while 'continue' in response:
-    url = url + '&blcontinue='+ response['continue']['blcontinue']
+def get_whatlinks(itemid):
+    whatLinks = []
+    url = f"https://www.wikidata.org/w/api.php?action=query&format=json&list=backlinks&bltitle={itemid}&bllimit=500&blnamespace=0"
     response = requests.get(url=url).json()
     whatLinks.extend(response['query']['backlinks'])
 
-QswhatLinks = [v['title'] for v in whatLinks]
-QswhatLinks = set(QswhatLinks)
+    while 'continue' in response:
+        url = url + '&blcontinue='+ response['continue']['blcontinue']
+        response = requests.get(url=url).json()
+        whatLinks.extend(response['query']['backlinks'])
 
+    QswhatLinks = (v['title'] for v in whatLinks)
+    QswhatLinks = set(QswhatLinks)
+    return(Qswhatlinks)
 
 #Complementary approach using SPARQL
 #TODO: Double check if there are pages appearing that were not included in the whatlinks approach
+def get_all_statements(itemid):
+    #https://w.wiki/KvX (Thanks User:Dipsacus_fullonum)
+    # All statements with item, property, value and rank with COVID-19 (Q84263196) as value for qualifier.
 
-sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
-#https://w.wiki/KvX (Thanks User:Dipsacus_fullonum)
-# All statements with item, property, value and rank with COVID-19 (Q84263196) as value for qualifier.
+    sparql.setQuery(f"""
+    SELECT ?item ?itemLabel ?property ?propertyLabel ?value ?valueLabel ?rank ?qualifier ?qualifierLabel
+    WHERE
+    {{
+      ?item ?claim ?statement.
+      ?property wikibase:claim ?claim.
+      ?property wikibase:statementProperty ?sprop.
+      ?statement ?sprop ?value.
+      ?statement wikibase:rank ?rank. 
+      ?statement ?qprop wd:{itemid}. # COVID-19
 
-sparql.setQuery("""
-SELECT ?item ?itemLabel ?property ?propertyLabel ?value ?valueLabel ?rank ?qualifier ?qualifierLabel
-WHERE
-{
-  ?item ?claim ?statement.
-  ?property wikibase:claim ?claim.
-  ?property wikibase:statementProperty ?sprop.
-  ?statement ?sprop ?value.
-  ?statement wikibase:rank ?rank. 
-  ?statement ?qprop wd:Q84263196. # COVID-19
 
-  
-  ?qualifier wikibase:qualifier ?qprop.
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
-}
-""")
-sparql.setReturnFormat(JSON)
-results = sparql.query().convert()
+      ?qualifier wikibase:qualifier ?qprop.
+      SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }}
+    }}
+    """)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
 
-allStatements = pd.io.json.json_normalize(results['results']['bindings'])
+    allStatements = pd.io.json.json_normalize(results['results']['bindings'])
+    allStatementsQ = [ link.split('/')[-1] for link in allStatements['item.value'].tolist()]
+    return allStatementsQ
+
 # All truthy statements with COVID-19 (Q84263196) as value.
 #https://w.wiki/KvZ (Thanks User:Dipsacus_fullonum)
+def get_truthy_statements(itemid):
+    sparql.setQuery(f"""
+    SELECT ?item ?itemLabel ?property ?propertyLabel
+    WHERE
+    {{
+      ?item ?claim wd:{itemid}.
+      ?property wikibase:directClaim ?claim.
+       SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }}
+    }}""")
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    truthy = pd.io.json.json_normalize(results['results']['bindings'])
+    truthyQ = [ link.split('/')[-1] for link in truthy['item.value'].tolist()]
 
-sparql.setQuery("""
-SELECT ?item ?itemLabel ?property ?propertyLabel
-WHERE
-{
-  ?item ?claim wd:Q84263196.
-  ?property wikibase:directClaim ?claim.
-   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
-}""")
-sparql.setReturnFormat(JSON)
-results = sparql.query().convert()
-truthy = pd.io.json.json_normalize(results['results']['bindings'])
-truthyQ = [ link.split('/')[-1] for link in truthy['item.value'].tolist()]
+    return truthyQ
 
-#Parse Q values from Wikidata
-allStatementsQ = [ link.split('/')[-1] for link in allStatements['item.value'].tolist()]
-allSPARQL = set(truthyQ).union(set(allStatementsQ))
+def get_statements(itemid):
 
-Q84263196AllItems  = allSPARQL.union(QswhatLinks)
+    sources = [get_truthy_statements,
+               get_all_statements,
+               get_whatlinks]
 
-
-# # 2019–20 COVID-19 pandemic (Q81068910)
-# What links here ppaorach
-url = 'https://www.wikidata.org/w/api.php?action=query&format=json&list=backlinks&bltitle=Q81068910&bllimit=500&blnamespace=0'
-response = requests.get(url=url).json()
-whatLinks.extend(response['query']['backlinks'])
-
-while 'continue' in response:
-    url = url + '&blcontinue='+ response['continue']['blcontinue']
-    response = requests.get(url=url).json()
-    whatLinks.extend(response['query']['backlinks'])
-
-QswhatLinks2 = [v['title'] for v in whatLinks]
-QswhatLinks2 = set(QswhatLinks)
-
-#All truthy statements with 2019–20 COVID-19 pandemic (Q81068910) as value.
-#https://w.wiki/Kvd (Thanks User:Dipsacus_fullonum)
-
-sparql.setQuery("""
-# 
-SELECT ?item ?itemLabel ?property ?propertyLabel WHERE {
-  ?item ?claim wd:Q81068910. #2019–20 COVID-19 pandemic
-  ?property wikibase:directClaim ?claim.
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
-}
-""")
-sparql.setReturnFormat(JSON)
-results = sparql.query().convert()
-
-Q81068910 = pd.io.json.json_normalize(results['results']['bindings'])
-Q81068910SPARQL = [ link.split('/')[-1] for link in Q81068910['item.value'].tolist()]
-Q81068910SPARQL = set(Q81068910SPARQL)
-
-# join whatlinks and SPARQL obtained pages
-
-Q81068910All = Q81068910SPARQL.union(QswhatLinks2)
-
-# Joining the full set from both seeds
-Qs = Q81068910All.union(Q84263196AllItems)
-
-## add seeds (by definition they were out)
-Qs = Qs.union({'Q81068910','Q84263196','Q82069695'})
+    return(reduce(set.union, (src(itemid) for src in sources),set()))
 
 
-
-# Adding a third seed  SARS-CoV-2 (Q82069695)
-url = 'https://www.wikidata.org/w/api.php?action=query&format=json&list=backlinks&bltitle=Q82069695&bllimit=500&blnamespace=0'
-response = requests.get(url=url).json()
-whatLinks.extend(response['query']['backlinks'])
-
-while 'continue' in response:
-    url = url + '&blcontinue='+ response['continue']['blcontinue']
-    response = requests.get(url=url).json()
-    whatLinks.extend(response['query']['backlinks'])
-
-QswhatLinks3 = [v['title'] for v in whatLinks]
-QswhatLinks3 = set(QswhatLinks)
-
-
-Qs = Qs.union(QswhatLinks3) 
-
+def get_statements_for_ids(item_ids):
+    found_Qs = reduce(set.union, map(get_all_statements, item_ids),set())
+    return found_Qs.union(set(item_ids))
 
 ### Getting articles and relation
 
@@ -156,17 +107,10 @@ def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
-        
-        
-wikidata_query_base = 'https://www.wikidata.org/w/api.php?action=wbgetentities&format=json&ids=' 
-itemsInfo = {}
-c = 0
-for items in chunks(list(Qs),50):
-    c +=50
-    if c%200 ==0: print(c,'items reviewed')
-    url = wikidata_query_base  + '|'.join(items)
-    itemsInfo.update(requests.get(url=url).json()['entities'])
 
+def get_item_infos(items):
+    url = wikidata_query_base  + '|'.join(items)
+    return requests.get(url=url).json().get('entities',dict())
 
 #Define parsing functions
 def getRelationships(claims,targetQs): #TODO change relationship to relation
@@ -219,114 +163,104 @@ def getValueIfWikidataItem(claim):
     return output
     
 
-## Parse result and build pandas dataframes
+if __name__=="__main__":
+    Qs = get_statements_for_ids({'Q81068910','Q84263196','Q82069695'})
 
-pagesPerProject = {}
-pagesPerProjectTable = {}
-itemsInfoTable = {}
-labelsEn = {}
-for item,v in itemsInfo.items():
-    itemsInfoTable[item] = {}
-    try:
-        itemsInfoTable[item]['item_Label'] = v['labels']['en']['value']
-    except:
-        itemsInfoTable[item]['item_Label'] = 'unknown '
-    #checking if there are claims for that Q, if not claims we return an empty dict, to avoid errors
-    claims = v.get('claims',{})
-    if 'P31' in  claims: #getting part of to classify the item        
-        itemsInfoTable[item]['Instace_Of'] = getValueIfWikidataItem(claims.get('P31'))
-    else:
-        itemsInfoTable[item]['Instace_Of'] = ['unknown']
-    #find COVID-19 / COVID-19 pandemics relationships
-    itemsInfoTable[item]['RelationTuple'] = getRelationships(claims,['Q81068910','Q84263196'])
+    chunked_Qs = list(chunks(list(Qs),50))
 
-    if 'sitelinks' in v:
-        for wiki,data in v['sitelinks'].items():
-            page = data['title']
-            project ='%s.wikipedia' % wiki.replace('wiki','')
-            pagesPerProject[project] = pagesPerProject.get(project,[])
-            pagesPerProject[project].append(page)
-            article_link = 'https://%s.org/wiki/%s' % (project,page)
-            projectcode = project.split('.')[0]
-            wikilink = '[[%s:%s|%s]]' % (projectcode,page,page)
-            pagesPerProjectTable[article_link] = {'project':project,'page':page,'wikidataItem':item,'wikilink':wikilink}
-            
+    print("fetching item infos")
 
-#Build     itemsInfoTable        
-itemsInfoTable = pd.DataFrame.from_dict(itemsInfoTable,orient='index')
-itemsInfoTable['last_seen'] = now
-itemsInfoTable = itemsInfoTable.explode('Instace_Of').explode('RelationTuple') 
-itemsInfoTable['connector'] = itemsInfoTable['RelationTuple'].apply(lambda x:x[0])
-itemsInfoTable['connected_To'] = itemsInfoTable['RelationTuple'].apply(lambda x:x[1])
-itemsInfoTable.drop('RelationTuple',inplace=True,axis=1)
+    # {**d1,**d2} evaluates to an updated dictionary with values from d2 replacing those from d1.
+    # tqdm is an easy lightweight progress bar
+    itemsInfo = reduce(lambda d1,d2: {**d1,**d2}, map(get_item_infos, tqdm(chunked_Qs)), dict())
+    ## Parse result and build pandas dataframes
+    pagesPerProject = {}
+    pagesPerProjectTable = {}
+    itemsInfoTable = {}
+    labelsEn = {}
+    for item,v in itemsInfo.items():
+        itemsInfoTable[item] = {}
+        try:
+            itemsInfoTable[item]['item_Label'] = v['labels']['en']['value']
+        except:
+            itemsInfoTable[item]['item_Label'] = 'unknown '
+        #checking if there are claims for that Q, if not claims we return an empty dict, to avoid errors
+        claims = v.get('claims',{})
+        if 'P31' in  claims: #getting part of to classify the item        
+            itemsInfoTable[item]['Instace_Of'] = getValueIfWikidataItem(claims.get('P31'))
+        else:
+            itemsInfoTable[item]['Instace_Of'] = ['unknown']
+        #find COVID-19 / COVID-19 pandemics relationships
+        itemsInfoTable[item]['RelationTuple'] = getRelationships(claims,['Q81068910','Q84263196'])
 
-
-#
-pagesPerProjectTable = pd.DataFrame.from_dict(pagesPerProjectTable,orient='index')
-pagesPerProjectTable['last_seen'] = now
-pagesPerProjectTable['url'] = pagesPerProjectTable.index
+        if 'sitelinks' in v:
+            for wiki,data in v['sitelinks'].items():
+                page = data['title']
+                project ='%s.wikipedia' % wiki.replace('wiki','')
+                pagesPerProject[project] = pagesPerProject.get(project,[])
+                pagesPerProject[project].append(page)
+                article_link = 'https://%s.org/wiki/%s' % (project,page)
+                projectcode = project.split('.')[0]
+                wikilink = '[[%s:%s|%s]]' % (projectcode,page,page)
+                pagesPerProjectTable[article_link] = {'project':project,'page':page,'wikidataItem':item,'wikilink':wikilink}
 
 
+    #Build     itemsInfoTable        
+    itemsInfoTable = pd.DataFrame.from_dict(itemsInfoTable,orient='index')
+    itemsInfoTable['last_seen'] = now
+    itemsInfoTable = itemsInfoTable.explode('Instace_Of').explode('RelationTuple') 
+    itemsInfoTable['connector'] = itemsInfoTable['RelationTuple'].apply(lambda x:x[0])
+    itemsInfoTable['connected_To'] = itemsInfoTable['RelationTuple'].apply(lambda x:x[1])
+    itemsInfoTable.drop('RelationTuple',inplace=True,axis=1)
+
+    #
+    pagesPerProjectTable = pd.DataFrame.from_dict(pagesPerProjectTable,orient='index')
+    pagesPerProjectTable['last_seen'] = now
+    pagesPerProjectTable['url'] = pagesPerProjectTable.index
+
+    connectedToLabel = {'Q84263196':'COVID-19', 'Q81068910':'2019–20 COVID-19 pandemic'} 
+    itemsInfoTable['connected_To_Label'] = itemsInfoTable['connected_To'].apply(lambda x:connectedToLabel.get(x))
+
+    ## Getting labels for connector (properties)
+    Ps = list(itemsInfoTable['connector'].unique())
+    props = []
+    for P in Ps:
+        props.append(requests.get('https://www.wikidata.org/w/api.php?action=wbgetentities&ids=%s&format=json' % P).json())
+    propLabels ={}
+    for P in props:
+        if 'entities' in P:
+            for Pid,data in P['entities'].items():
+                tmplabel = data.get('labels').get('en',{})
+                propLabels[Pid]= tmplabel.get('value','unknown')
+    propLabels = pd.DataFrame.from_dict(propLabels,orient='index',columns=['connector_Label'])
+    propLabels['connector'] = propLabels.index
+
+    #adding labels to itemsInfoTable
+    itemsInfoTable = itemsInfoTable.join(propLabels, on='connector',rsuffix='_tmp').drop('connector_tmp',axis=1)
+    itemsInfoTable['item_id'] = itemsInfoTable.index
 
 
+    ## Getting Instance of labels
+    instaceOfQs = list(itemsInfoTable['Instace_Of'].unique())
+    print(len(instaceOfQs))
+    QiOf = [] # Q instace
+    for Q in instaceOfQs:
+        QiOf.append(requests.get('https://www.wikidata.org/w/api.php?action=wbgetentities&ids=%s&format=json' % Q).json())
+    QiOfLabels ={}
+    for P in QiOf:
+        if 'entities' in P:
+            for Pid,data in P['entities'].items():
+                tmplabel = data.get('labels').get('en',{})
+                QiOfLabels[Pid]= tmplabel.get('value','unknown')
+    QiOfLabels = pd.DataFrame.from_dict(QiOfLabels,orient='index',columns=['Instace_Of_Label'])
+    QiOfLabels['Instace_Of'] = QiOfLabels.index
+
+    #FINALVERSION Of Info Table
+    itemsInfoTable = itemsInfoTable.join(QiOfLabels, on='Instace_Of',rsuffix='_tmp').drop('Instace_Of_tmp',axis=1)
 
 
-
-
-connectedToLabel = {'Q84263196':'COVID-19', 'Q81068910':'2019–20 COVID-19 pandemic'} 
-itemsInfoTable['connected_To_Label'] = itemsInfoTable['connected_To'].apply(lambda x:connectedToLabel.get(x))
-
-
-# In[85]:
-
-
-## Getting labels for connector (properties)
-Ps = list(itemsInfoTable['connector'].unique())
-props = []
-for P in Ps:
-    props.append(requests.get('https://www.wikidata.org/w/api.php?action=wbgetentities&ids=%s&format=json' % P).json())
-propLabels ={}
-for P in props:
-    if 'entities' in P:
-        for Pid,data in P['entities'].items():
-            tmplabel = data.get('labels').get('en',{})
-            propLabels[Pid]= tmplabel.get('value','unknown')
-propLabels = pd.DataFrame.from_dict(propLabels,orient='index',columns=['connector_Label'])
-propLabels['connector'] = propLabels.index
-
-#adding labels to itemsInfoTable
-itemsInfoTable = itemsInfoTable.join(propLabels, on='connector',rsuffix='_tmp').drop('connector_tmp',axis=1)
-itemsInfoTable['item_id'] = itemsInfoTable.index
-
-
-## Getting Instance of labels
-instaceOfQs = list(itemsInfoTable['Instace_Of'].unique())
-print(len(instaceOfQs))
-QiOf = [] # Q instace
-for Q in instaceOfQs:
-    QiOf.append(requests.get('https://www.wikidata.org/w/api.php?action=wbgetentities&ids=%s&format=json' % Q).json())
-QiOfLabels ={}
-for P in QiOf:
-    if 'entities' in P:
-        for Pid,data in P['entities'].items():
-            tmplabel = data.get('labels').get('en',{})
-            QiOfLabels[Pid]= tmplabel.get('value','unknown')
-QiOfLabels = pd.DataFrame.from_dict(QiOfLabels,orient='index',columns=['Instace_Of_Label'])
-QiOfLabels['Instace_Of'] = QiOfLabels.index
-
-#FINALVERSION Of Info Table
-itemsInfoTable = itemsInfoTable.join(QiOfLabels, on='Instace_Of',rsuffix='_tmp').drop('Instace_Of_tmp',axis=1)
-
-
-# SAve on database
-conn = sqlite3.connect('/home/dsaez/real-time-wiki-covid-tracker/AllWikidataItems.sqlite')
-# first 
-itemsInfoTable.to_sql(name='itemsInfoTable', if_exists='replace', con=conn)
-pagesPerProjectTable.to_sql(name='pagesPerProjectTable', if_exists='replace', con=conn)
-
-
-
-
-
-
-
+    # SAve on database
+    conn = sqlite3.connect('./AllWikidataItems.sqlite')
+    # first 
+    itemsInfoTable.to_sql(name='itemsInfoTable', if_exists='replace', con=conn)
+    pagesPerProjectTable.to_sql(name='pagesPerProjectTable', if_exists='replace', con=conn)
